30376838b3752089f382c7c70200ecb5e936b67f
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2022-03-26 13:50:52 -0300

    feat(js): All Features Implemented

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,24 @@
 import { Route, Switch } from "react-router-dom";
 import Register from "../pages/Register";
 import Leads from "../pages/Leads";
+import { Redirect } from "react-router-dom";
+
+const LeadsRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={(props) =>
+            !!localStorage.getItem("Usuário") ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/register" />
+            )
+        }
+    />
+);
 
 const Router = () => (
     <Switch>
-        <Route exact path="/" component={() => <Leads />} />
+        <LeadsRoute exact path="/" component={() => <Leads />} />
         <Route path="/register" component={() => <Register />} />
     </Switch>
 );
